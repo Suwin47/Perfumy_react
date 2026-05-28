bb80cbbcfefdde9b11a31bff50e4b4e666364458
Suvin S <your-email@example.com>
2026-05-28 14:41:23 +0530

    updated file

---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+import one from "../assets/images/one.jpg";
+import two from "../assets/images/two.jpg";
+import three from "../assets/images/three.jpg";
+
+//Product component
+function Product() {
+  return (
+  <div className="products">
+            <div className="box">
+                        <img src={one} alt="Fogg Wood Extreme"></img>
+                        <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p>    
+            </div>
+            <div className="box">
+                <img src={two} alt="villain"></img>
+                <p>Fogg Wood Extreme, Eau De Parfum, Long-lasting Perfume, 100ml</p> 
+            </div>
+            <div className="box">
+                <img src={three} alt="Designs Club"></img>
+                <p>Designer Club-Be Spoke by Perfume Lounge Men's Perfume | Premium Long Lasting Perfume</p>
+            </div>
+        </div>
+  );
+}
+
+export default Product;
